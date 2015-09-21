0472427d32ed5c79491f3751df71767acb9d41c5
Jordan Nicholas Simon <jsimon@sapient.com>
2015-09-21 15:24:07 -0700

    Updating boilerplate to use Gulp and Browserify

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserify = require('gulp-browserify');
+var sourcemaps = require('gulp-sourcemaps');
+
+var sassSources = 'styles/**/*.scss';
+var jsSources = 'scripts/**/*.js';
+
+gulp.task('styles', function() {
+    gulp.src(sassSources)
+    	.pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('styles/css/'));
+});
+
+gulp.task('scripts', function(){
+	gulp.src(jsSources)
+		.pipe(browserify({ debug: true }))
+		.pipe(gulp.dest('build/js'));
+});
+
+//Watch task
+gulp.task('watch', function() {
+	gulp.watch(jsSources,['scripts']);
+    gulp.watch(sassSources,['styles']);
+});
+
+gulp.task('default', ['styles', 'scripts', 'watch']);
