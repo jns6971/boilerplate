7e187dc746d94366981feaf412743ececf6b6543
Jordan Simon <Jordan Simon>
2016-04-22 14:25:55 -0700

    added details to Ascena work carousel is still a work in process

--- scripts/main.js
+++ scripts/main.js
@@ -83,22 +83,22 @@ $(document).ready(function(){
 	// 	}
 	// });
 
-	$(".owl-carousel").owlCarousel({
-		'loop': true,
-		'margin': 10,
-		'nav': false,
-		responsive:{
-			0:{
-				'items':1
-			},
-			425:{
-				'items':2
-			},
-			768:{
-				'items':4
-			}
-		}
-	});
+	// $(".owl-carousel").owlCarousel({
+	// 	'loop': true,
+	// 	'margin': 10,
+	// 	'nav': true,
+	// 	responsive:{
+	// 		0:{
+	// 			'items':1
+	// 		},
+	// 		425:{
+	// 			'items':2
+	// 		},
+	// 		768:{
+	// 			'items':4
+	// 		}
+	// 	}
+	// });
 
 
 });
