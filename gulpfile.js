23a837428089229169f195a296aa1c92027d0a45
Jordan Simon <Jordan Simon>
2016-04-27 17:32:02 -0700

    Update gulp distribution process Finish carousel

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,24 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var browserify = require('gulp-browserify');
-var sourcemaps = require('gulp-sourcemaps');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserify = require('gulp-browserify'),
+    sourcemaps = require('gulp-sourcemaps'),
+    uglify = require('gulp-uglify'),
+    clean = require('gulp-clean'),
+    minifyCSS = require('gulp-minify-css'),
+    //rename = require('gulp-rename'),
+    htmlmin = require('gulp-htmlmin'),
+    autoprefixer = require('gulp-autoprefixer'),
+    imageOpt = require('gulp-image-optimization'),
+    gutil = require( 'gulp-util'),
+    ftp = require( 'vinyl-ftp');
 
-var sassSources = 'styles/sass/**/*.scss';
-var jsSources = 'scripts/**/*.js';
+var credentials = require('./server-cred.json');
+
+var jsSources = 'scripts/**/*.js',
+    sassSources = 'styles/sass/**/*.scss',
+    cssSources = 'styles/css/**/*.css',
+    htmlSources = '**/*.html',
+    imageSources = ['img/**/*.png','img/**/*.jpg','img/**/*.gif','img/**/*.jpeg'];
 
 gulp.task('styles', function() {
     gulp.src(sassSources)
@@ -25,5 +39,73 @@ gulp.task('watch', function() {
 	gulp.watch(jsSources,['scripts']);
     gulp.watch(sassSources,['styles']);
 });
+
+//minify js
+gulp.task('js-minify', function () {
+   gulp.src(jsSources)
+      .pipe(uglify())
+      .pipe(gulp.dest('dist/scripts'));
+});
+
+//minify css
+gulp.task('css-minify', function () {
+    gulp.src(cssSources)
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('dist/styles/css'));
+});
+
+gulp.task('html-minify', function() {
+  gulp.src(htmlSources)
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('copy', function () {
+    gulp.src(['index.html', '*.pdf'])
+        .pipe(gulp.dest('dist'));
+
+    gulp.src('fonts/*')
+        .pipe(gulp.dest('dist/fonts'));
+
+});
+
+//optimize images
+gulp.task('images', function(cb) {
+    gulp.src(imageSources).pipe(imageOpt({
+        optimizationLevel: 5,
+        progressive: true,
+        interlaced: true
+    })).pipe(gulp.dest('dist/img')).on('end', cb).on('error', cb);
+});
+
+gulp.task( 'deploy', function () {
+
+
+    var conn = ftp.create( {
+        'host': credentials.host,
+        'user': credentials.user,
+        'password': credentials.password,
+        'parallel': 10,
+        'log': gutil.log
+    });
+
+    var globs = [
+        'dist/**'
+    ];
+
+    return gulp.src( globs, { base: './dist', buffer: false } )
+        .pipe( conn.newer( '' ) ) // only upload newer files 
+        .pipe( conn.dest( '' ) );
+});
+
+//clean dist folder
+gulp.task('clean', function () {
+   gulp.src('dist', {'read': false})
+      .pipe(clean());
+});
+
+
+//Watch task
+gulp.task('dist', ['clean', 'copy', 'js-minify', 'css-minify', 'images']);
 
 gulp.task('default', ['styles', 'scripts', 'watch']);
--- scripts/main.js
+++ scripts/main.js
@@ -83,22 +83,11 @@ $(document).ready(function(){
 	// 	}
 	// });
 
-	// $(".owl-carousel").owlCarousel({
-	// 	'loop': true,
-	// 	'margin': 10,
-	// 	'nav': true,
-	// 	responsive:{
-	// 		0:{
-	// 			'items':1
-	// 		},
-	// 		425:{
-	// 			'items':2
-	// 		},
-	// 		768:{
-	// 			'items':4
-	// 		}
-	// 	}
-	// });
+	$(".owl-carousel").owlCarousel({
+		'loop': true,
+		'margin': 10,
+		'nav': true
+	});
 
 
 });
